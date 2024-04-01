46ab01ca80f303cfe2e0a53f5d57daa6cdae0a8b
Mandoiu Vlad - Stefan <vlad.mandoiu@student.unitbv.ro>
2024-04-01 13:42:48 +0300

    add files

---
+++ tema2_MVP_Dame/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace tema2_MVP_Dame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ tema2_MVP_Dame/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema2_MVP_Dame
+{
+    internal class Player
+    {
+        
+    }
+}
